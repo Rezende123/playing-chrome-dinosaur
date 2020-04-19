d0bbde2eddf619aa7d4e84e55bc6174b51389f31
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-18 22:57:51 -0300

    Definindo os controles

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ async function navigate() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     
-    await page.goto('https://chromedino.com/');    
+    await page.goto('https://chromedino.com/');
+
+    console.log('Entrou na página'); 
+
+    await page.keyboard.press('ArrowUp');
+
+    console.log('Pulou'); 
+
+    await page.keyboard.press('ArrowDown', { delay: 300 });
+
+    console.log('Abaixou'); 
 }
 
 navigate();
